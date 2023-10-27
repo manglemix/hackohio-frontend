0d94e92dc8fd21947a2ec6535fdb303e9ce4d345
Najman <najman.husaini@utah.edu>
2023-10-27 16:35:21 -0600

    corrected pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,9 @@
 <header>
-    <h1>Want to find patterns in littering?</h1>
-    <h2>We've got you covered!</h2>
+    <h1>About</h1>
 </header>
 <main>
     <div style="margin-top: 4rem;" />
-    <h3>There will be a heatmap here</h3>
+    <h3>Some info about us</h3>
 </main>
 
 <style>
@@ -12,10 +11,5 @@
         font-size: 3rem;
         font-weight: bolder;
         margin-top: 1rem;
-    }
-    h2 {
-        font-size: 1.5rem;
-        font-weight: normal;
-        margin-top: 0.5rem;
     }
 </style>
--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,9 +1,10 @@
 <header>
-    <h1>About</h1>
+    <h1>Want to find patterns in littering?</h1>
+    <h2>We've got you covered!</h2>
 </header>
 <main>
     <div style="margin-top: 4rem;" />
-    <h3>Some info about us</h3>
+    <h3>There will be a heatmap here</h3>
 </main>
 
 <style>
@@ -11,5 +12,10 @@
         font-size: 3rem;
         font-weight: bolder;
         margin-top: 1rem;
+    }
+    h2 {
+        font-size: 1.5rem;
+        font-weight: normal;
+        margin-top: 0.5rem;
     }
 </style>
